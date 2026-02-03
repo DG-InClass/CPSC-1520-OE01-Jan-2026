7df5a91a0a8e208953166b7117f4f7762aba94d9
Dan Gilleland <dgilleland@nait.ca>
2026-02-02 20:31:41 -0700

    Explain the code with comments

--- src/008/ultra-soft-intro/js/main.js
+++ src/008/ultra-soft-intro/js/main.js
@@ -1,8 +1,16 @@
-let counter = 0;
+// A "global" variable called counter
+let counter = 0; // initializing it to zero
+
+// Creating a function using Function Expression
 const increment = function() {
-    counter++;
+    counter++;  // counter = counter + 1
     document.querySelector('div').innerText = `Clicked ${counter} times.`;
 }
 
+// I get a reference to <button>Click Me</button>
 const theButton = document.querySelector('button');
+
+// Adding an Event Listener.
+// When the 'click' event happens on theButton,
+// the browser will call my increment function.
 theButton.addEventListener('click', increment);
