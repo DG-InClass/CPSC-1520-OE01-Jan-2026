54b57465ba1414b5284b776dc93ef4c367363694
Dan Gilleland <dgilleland@nait.ca>
2026-02-02 21:38:53 -0700

    Completed the soft intro to events
    Though it was a soft intro, it was packed with information!

--- src/008/soft-intro/js/main.js
+++ src/008/soft-intro/js/main.js
@@ -11,18 +11,48 @@
 console.log('JavaScript file loaded!');
 
 const handleClick = function(event) {
+    // If you want to STOP the default behaviour that the
+    // browser uses for an event, you can call
+    event.preventDefault();
+
     // TODO: Use for exploration purposes
-    const target = event.target;
+    const target = event.target; // What "received" the event
+    //    \____/  this will always be some HTMLElement
     document.getElementById('feedback').innerText = `Clicked from ${target.tagName}`;
+    // All HTMLElement objects have a .tagName property
 }
 
 let heading = document.querySelector('h1');
 heading.addEventListener('click', handleClick);
+//                       \_____/  \_________/
+// The event event name is  |        |
+// always as a string                |
+//                The second argument is an actual function
+//                that will be called when the even happens
+
+
 
 // TODO: Add an event listener for the button with id "register".
 //       Listen for the 'dblclick' event.
 //       Prompt the user for their name, then for their email,
 //       then output the information to the #feedback element.
+let theButton = document.getElementById('register');
+theButton.addEventListener('dblclick', function(evt) {
+    // This is an inline and anonymous function
+    console.log(evt); // What's the event object?
+
+    const aName = prompt('Tell me your name:');
+    const email = prompt('What is your email?');
+    const output = document.getElementById('feedback');
+    output.innerText = `Hello ${aName}. I have shared your email with people on the dark web.... (${email})`;
+});
+
+// Ad-hoc demo: Listen to clicks on the body
+document.body.addEventListener('click', function() {
+    let output = document.getElementById('feedback');
+    output.innerHTML += '<br />I heard that!';
+});
+
 
 // TODO: Add another event listener for the heading, this time for
 //       the 'dblclick' event.
@@ -30,3 +60,21 @@ heading.addEventListener('click', handleClick);
 //       to the following element's property:
 //          document.querySelector('html').dataset.theme
 //       Try it out, then inspect the elements in the Dev Tools.
+heading.addEventListener('dblclick', function(ev) {
+    ev.preventDefault();
+    ev.stopPropagation(); // Google what this does
+    let htmlTag = document.querySelector('html');
+    let currentTheme = htmlTag.dataset.theme;
+    // Bonus! An intro to if/else statements....
+    if(currentTheme === 'dark')
+        htmlTag.dataset.theme = 'light';
+    else
+        htmlTag.dataset.theme = 'dark';
+});
+
+// Can you describe the difference between the
+// .stopPropagation() and .preventDefault() methods
+// that exist on the Event object that's passed into
+// an event handler?
+document.body.innerHTML += '<a href="https://dgilleland.github.io/CPSC-1520/tutorials/0070/" target="_blank">Event Listeners</a>';
+// What happens if you add a 'click' event handler to the link above and perform a call to the event's .stopPropagation()?
