661437d0ab03c477eb9a2dab49e4f4c2e85c24a7
Dan Gilleland <dgilleland@nait.ca>
2026-02-09 20:45:15 -0700

    A few text manipulation + button click demos

--- src/008/demo-text-manipulation/js/main.js
+++ src/008/demo-text-manipulation/js/main.js
@@ -1,6 +1,11 @@
 // Some global variables that we will re-use frequently
+// <input type="text" id="userInput" value="Lorem ipsum dolor sit amet">
 const userInput = document.getElementById('userInput');
+
+// <pre id="transformedText"></pre>
 const output = document.getElementById('transformedText');
+
+
 // Show the user input in the output
 output.innerText = userInput.value;
 // A little prep....
@@ -13,19 +18,33 @@ presetPaddingLength();
 // And now for the lesson....
 // Demo: 0) Completed demo to transform the user's input to upper case and display it
 document.getElementById('transformToUpperCase').addEventListener('click', function() {
+    // userInput    <input ..... />
     let newValue = userInput.value.toUpperCase();
-    //             \  string     /
+    //    /\       \  string     /\__ string___/
+    //     |                           |
+    //     |---------------------------|
     output.innerText = newValue;
 });
 
 // TODO: 1) Write the code to transform the user's input to lower case and display it
 //       STUDENT_CODE_HERE
+document                                    // the entire DOM page
+    .querySelector('#transformToLowerCase') // <button>
+    .addEventListener('click', function() {
+        let text = userInput.value;
+        let lowerCaseText = text.toLowerCase();
+        output.innerText = lowerCaseText;
+    });
+
 
 // Demo: 2) Write the code to pad the end of input text with the supplied character(s).
 // TODO:    Modify the code below to use the appropriate user input for padding.
 document.querySelector('#transformPadEnd').addEventListener('click', function() {
-    let currentValue = 'bob';// userInput.value;
-    let newValue = currentValue.padEnd(10, '^');
+    let currentValue = userInput.value;             // ✅
+    let minLengthInput = document.querySelector('#minLength');
+    let minLen = parseInt(minLengthInput.value);
+    let char = document.getElementById('endText').value;
+    let newValue = currentValue.padEnd(minLen, char);
     output.innerText = newValue;
 });
 
