67b43c28ef9b49cb9f667927f93aa7c48890c2fa
Dan Gilleland <dgilleland@nait.ca>
2026-02-09 21:46:01 -0700

    Intro to Form Processing

--- src/008/form-processing/js/main.js
+++ src/008/form-processing/js/main.js
@@ -11,8 +11,8 @@ const clearFeedback = function(text) {
 document
     .getElementById('subscribe') // Get the <form id="subscribe"> element
     .addEventListener('submit', function(ev) { // an anonymous function responding to submit
-        ev.preventDefault(); // This will stop the browser from submitting the form to the server
-        // console.log(ev);
+        ev.preventDefault(); // 📌 This will stop the browser from submitting the form to the server
+        console.log(ev);
         let theForm = ev.target;
         clearFeedback('The subscribe form was submitted.');
         let firstNameInput = theForm.elements.firstname;
@@ -20,6 +20,8 @@ document
         let emailInput = theForm.elements.email;
         appendFeedback(`Their email is: '${emailInput.value}'`);
 
+        // <input name="terms" type="checkbox" />
+        // theForm.element.terms   has a property called .checked
         appendFeedback(`Agreed to terms? '${theForm.elements.terms.checked}'`);
         console.log(theForm.elements.term); // see the <input type="checkbox" in the console
     });
@@ -27,14 +29,42 @@ document
 
 let otherForm = document.querySelector('#assorted');
 const exploreForm = function(ev) {
-    ev.preventDefault();
+    ev.preventDefault(); // 📌 Always call .preventDefault()
+
     clearFeedback('This is the data from the assorted form:');
-    let target = ev.target;
-    let theInputs = target.elements;
+    
+    let target = ev.target; // <form id="assorted">....
+    let theInputs = target.elements; // All of the user controls
     console.log('theInputs: ', theInputs);
 
+    let dateInput = theInputs.date; // <input type="date" name="date" >
+    let dateValue = dateInput.value; // data type of .value is string
+    let message = `Today is ${dateValue}`; // What will it be
+    appendFeedback(message);
+    let dateObj = new Date(dateValue);
+    message = `The date as a Date object is ${dateObj.toDateString()}`;
+    appendFeedback(message);
+
+    let timeInput = theInputs.time; // 
+    let theTime = timeInput.value;  // theTime is a string
+    message = `The time supplied was ${theTime}`;
+    appendFeedback(message); // 🤦‍♂️ d'oh
+    console.log(theTime, 'split on the :', theTime.split(':'));
+
+    // Playing with date/time information
+    let year = dateObj.getFullYear(); // 4-digit year
+    let month = dateObj.getMonth();   // 0-11
+    let dayOfMonth = dateObj.getDate();
+    let timeParts = theTime.split(':'); // array of two strings
+    let hours = parseInt(timeParts[0]);
+    let minutes = parseInt(timeParts[1]);
+    // Create a new Date object based on these two <input> controls
+    let dateAndTime = new Date(year, month, dayOfMonth, hours, minutes);
+    message = `\nMy combined date and time produces:\n\t${dateAndTime}`;
+    appendFeedback(message);
+
     // If your input control has a name with a dash in it,
-    // use the name's value as an "index" for the object.
+    // use the name's value as an "index" for the object. 🤔
     let theBackgroundColorInput = theInputs['bg-color'];
     // for <input type="radio" name="bg-color" value="#ff0000" />
 
