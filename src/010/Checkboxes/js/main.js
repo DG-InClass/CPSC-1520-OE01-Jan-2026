da86bf43ad27dcbff1682b29101f1281c9334b3e
Dan Gilleland <dgilleland@nait.ca>
2026-03-02 20:14:00 -0700

    Checkboxes demo

--- src/010/Checkboxes/js/main.js
+++ src/010/Checkboxes/js/main.js
@@ -1,1 +1,26 @@
 // Enter JavaScript for the exercise here...
+// [name=toggle]
+// [name=message]
+
+const toggleAll = document.querySelector('[name=toggle]');
+toggleAll.addEventListener('change', function(evt) {
+    const target = evt.target;
+    console.log(`main toggle is ${target.checked ? 'on' : 'off'}`);
+
+    const allCheckboxes = document.querySelectorAll('[name=message');
+    console.log(allCheckboxes);
+
+    let index = 0;
+    while(index < allCheckboxes.length) {
+        // How would you change the following line so that
+        // it toggles the individual checkboxes instead of
+        // making all the checkboxes match the toggleAll's
+        // checked state?
+        // allCheckboxes[index].checked = target.checked;
+        allCheckboxes[index].checked = !allCheckboxes[index].checked;
+        //                             |\__________________________/
+        //                           NOT(       true/false         )
+        //                          <==       false/true
+        index += 1; // index = index + 1
+    }
+});
