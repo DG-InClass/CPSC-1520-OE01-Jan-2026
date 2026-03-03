9302b93f61723e043af0f5c94ceab1c21cdca0b3
Dan Gilleland <dgilleland@nait.ca>
2026-03-02 20:37:04 -0700

    Completed checkbox challenge

--- src/010/Checkboxes/js/main.js
+++ src/010/Checkboxes/js/main.js
@@ -24,3 +24,27 @@ toggleAll.addEventListener('change', function(evt) {
         index += 1; // index = index + 1
     }
 });
+
+
+document
+    .getElementById('search') // <form id="search" >
+    .addEventListener('submit', function(evt) {
+        evt.preventDefault(); // Always remember this for submit events
+        const searchText = evt.target.elements.searchText.value.toLowerCase().trim();
+        // debugger // The debugger triggers only when the browser dev tools is open
+        if(searchText) {
+            const allCells = document.querySelectorAll('tbody tr td:last-child'); // NodeList <td>
+            // All NodeList objects have a .forEach() method
+            allCells.forEach(cell => {
+                // cell is a <td> element
+                if(cell.innerText.toLowerCase().includes(searchText)) {
+                    cell.parentElement.classList.add('selected');
+                    // The parent of the <td> is the <tr> element
+                } else {
+                    cell.parentElement.classList.remove('selected');
+                }
+            });
+        } else {
+            // TODO: Remove the .selected class from all the rows of the table body
+        }
+    });
