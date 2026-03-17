d71e78f80e7406131e9f0bdc886c4f9f0c464754
Dan Gilleland <dgilleland@nait.ca>
2026-03-16 19:58:14 -0600

    Checking that fetch of books works

--- src/013/books/src/main.js
+++ src/013/books/src/main.js
@@ -1,13 +1,25 @@
 // Import the functions necessary to make the API calls
+import { fetchData } from './utils';
 
 // Select the necessary DOM elements
+const loadButton = document.getElementById('loadBooks');
+const list = document.getElementById('bookList');
 
 // Define the API endpoint
+const endpoint = 'http://localhost:3000/books'; // Note: This variable might be better to put in another file (depending on how complex/large your code starts to get in `main.js`)
 
 // Define a function to handle loading and displaying the list of books
+const loadHandler = function() {
+    fetchData(endpoint)
+        .then(data => {
+            // 😁 Looks like we got data parsed from JSON endpoint
+            console.log(data);
+        })
+}
 
 // Define a function to handle form submission for adding a new book
 
 // Attach event listeners to the button and form
+loadButton.addEventListener('click', loadHandler);
 
 // TODO: Add delete functionality
--- src/013/books/src/utils.js
+++ src/013/books/src/utils.js
@@ -1,4 +1,28 @@
 // Fetch utility function
+// ⚠️ NOTE: The version of `fetchData()` that I am showing here differs from the version shown in the `README.md` docs for this demo.
+export function fetchData(endpoint) {
+    return fetch(endpoint).then(handleResponse);
+    //     \_____________/
+    //       | Promise<Response>
+}
+
+/**
+ * Check that the response is ok (status code 200) and returns a
+ * JavaScript object from the JSON data.
+ * @param {Response} response - An HTTP Response from a web server
+ * @return {Promise<object>} - A promise of some JavaScript object
+ */
+const handleResponse = function(response) {
+    if(!response.ok) {
+        // Uh-oh, something went wrong
+        let message = `Server responsed with ${response.status} (${response.statusText}). (${response.url})`;
+        throw new Error(message);
+    }
+    // Since throwing an exception automatically exits a function,
+    // we don't need an "else" part to our "if" statement.
+    return response.json(); // Parse the body of the response as though it is a JSON string and return the JavaScript object/array.
+}
+
 
 // POST utility function
 
