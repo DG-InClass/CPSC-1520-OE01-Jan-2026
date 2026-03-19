0e6ee5c597d28e20f01828ffb17bde3939dfc29d
Dan Gilleland <dgilleland@nait.ca>
2026-03-18 19:40:30 -0600

    Quick intro to the template element

--- src/015/why-dom-api/js/main.js
+++ src/015/why-dom-api/js/main.js
@@ -7,10 +7,16 @@ theForm.addEventListener('submit', function(evt) {
     const html = evt.target.elements.htmlContent.value.trim();
     const container = document.getElementById('output');
     if(html) {
-        container.innerHTML = html; // 💣
+        container.innerHTML = html; // 💣 DANGEROUS to allow un-sanitized HTML to be inserted/injected into our page. It's a security risk.
     } else {
         const template = document.getElementById('default-content');
         const clone = template.content.cloneNode(true);
+
+        // Once I have a copy of the template, I can programmatically
+        // modify its contents however I need.
+        const asideEl = clone.querySelector('aside');
+        asideEl.innerText = `Today is ${new Date().toDateString()}`;
+
         container.innerHTML = '';
         container.appendChild(clone);
     }
